3199ed27840cbe7d03578c6237117e3d7993e840
Ubuntu <ubuntu@ip-172-31-34-255.ap-south-1.compute.internal>
2025-04-21 06:41:50 +0000

    limbo changes updated

--- module/bets/bet-db.js
+++ module/bets/bet-db.js
@@ -5,8 +5,7 @@ export const insertSettlement = async(data, matchMult)=> {
     try{
         const [initial, matchId, operatorId, userId, betAmount, multiplier] = data.split(':');
         const decodeUserId = decodeURIComponent(userId);
-        const winMult = matchMult >= Number(multiplier) ? Number(multiplier).toFixed(2) : 0.00 
-        await write(`INSERT INTO settlement (bet_id, lobby_id, user_id, operator_id, bet_amount, max_mult, match_max_mult) VALUES(?, ?, ?, ?, ?, ?, ?)`, [data, matchId, decodeUserId, operatorId, parseFloat(betAmount), winMult, matchMult]);
+        await write(`INSERT INTO settlement (bet_id, lobby_id, user_id, operator_id, bet_amount, max_mult, match_max_mult) VALUES(?, ?, ?, ?, ?, ?, ?)`, [data, matchId, decodeUserId, operatorId, parseFloat(betAmount), multiplier, matchMult]);
         console.log(`Settlement data inserted successfully`);
     }catch(err){
         console.error(`Err while inserting data in table is:::`, err);
